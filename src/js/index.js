8fb65e5bac538543312831e4894f6bdcaa5872bd
Eduardo Jose Maya Rodriguez <ediij1994@gmail.com>
2024-03-02 15:34:58 -0500

    modal carga valores de api

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,34 @@
 import { fetchAllMovies, fetchSameMovies, fetchMovieDetails } from './api.js';
+import { removeModal, toggleModal } from './modal-info.js';
 
-fetchAllMovies().then(data => console.log(data));
-fetchSameMovies('tears', 1);
+const galleryList = document.getElementById('gallery-list');
+const firstModal = document.querySelector('li[first-modal]');
+
+fetchAllMovies().then(data => {
+  console.log(data.results);
+  firstModal.classList.add('is-hidden');
+  data.results.forEach(element => {
+    galleryList.innerHTML += `
+    <li class="gallery__item">
+        <div class="gallery__container"  id=${element.id} data-modal-open="">
+            <img
+                class="gallery__img"
+                src="https://image.tmdb.org/t/p/original${element.poster_path}"
+                alt="${element.media_type}"
+            />
+            </div>
+                <div class="gallery__description">
+                <h3 class="gallery__title" style="font-style:uppercase">${element.title}</h3>
+                <p class="gallery__subtitle">${element.genre_ids} | </p>
+            </div>
+        </div>
+    </li>`;
+  });
+});
+
+galleryList.addEventListener('click', event => {
+  toggleModal(event.target.offsetParent.id);
+});
+/*fetchSameMovies('tears', 1);
 fetchMovieDetails(1217605);
+*/
--- src/js/modal-info.js
+++ src/js/modal-info.js
@@ -1,22 +1,50 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
-  };
+import { fetchMovieDetails } from './api';
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
-  window.addEventListener('keydown', event => {
-    if (event.code === 'Escape') {
-      removeModal();
-    }
-  });
+const refs = {
+  openModalBtn: document.querySelector('[data-modal-open]'),
+  closeModalBtn: document.querySelector('[data-modal-close]'),
+  modal: document.querySelector('[data-modal]'),
+  movieTitle: document.querySelector('#movie-title'),
+  vote: document.querySelector('#vote'),
+  votes: document.querySelector('#votes'),
+  popularity: document.querySelector('#popularity'),
+  ogTitle: document.querySelector('#og-title'),
+  genre: document.querySelector('#genre'),
+  sinopsis: document.querySelector('#sinopsis'),
+  poster: document.querySelector('#poster'),
+};
 
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
+refs.openModalBtn.addEventListener('click', toggleModal);
+refs.closeModalBtn.addEventListener('click', toggleModal);
+window.addEventListener('keydown', event => {
+  if (event.code === 'Escape') {
+    removeModal();
   }
-  function removeModal() {
-    refs.modal.classList.add('is-hidden');
-  }
-})();
+});
+
+export function toggleModal(id) {
+  refs.modal.classList.toggle('is-hidden');
+  fetchMovieDetails(id)
+    .then(response => {
+      console.log(response.genres.map(({ name }) => name));
+      refs.poster.src =
+        `https://image.tmdb.org/t/p/w400${response.poster_path}` || 'none';
+      refs.vote.textContent = response.vote_average || 'none';
+      refs.votes.textContent = response.vote_count || 'none';
+      refs.ogTitle.textContent = response.original_title || 'none';
+      refs.popularity.textContent = response.popularity || 'none';
+      refs.movieTitle.textContent = response.title || 'none';
+      refs.sinopsis.textContent = response.overview || 'none';
+      refs.genre.textContent = response.genres.map(({ name }) => name);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
+export function removeModal() {
+  refs.modal.classList.add('is-hidden');
+}
+/* 
+  poster_path: es el codigo de la imagen de poster
+  enlace para llamar imagen https://image.tmdb.org/t/p/original/${poster_path}
+  */
