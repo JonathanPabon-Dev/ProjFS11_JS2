f6728a8d3dee15f089e3f4c071c6d63fbbe8f854
Eduardo Jose Maya Rodriguez <ediij1994@gmail.com>
2024-03-03 22:04:15 -0500

    funcionalidad modales completa

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
-import { fetchAllMovies, fetchSameMovies, fetchMovieDetails } from './api.js';
-import { removeModal, toggleModal } from './modal-info.js';
+import { fetchAllMovies } from './api.js';
+import { toggleModal } from './modal-info.js';
 
 const galleryList = document.getElementById('gallery-list');
 const firstModal = document.querySelector('li[first-modal]');
@@ -7,35 +7,26 @@ const firstModal = document.querySelector('li[first-modal]');
 fetchAllMovies().then(data => {
   firstModal.classList.add('is-hidden');
   data.results.forEach(element => {
-    const genres = element.genre_ids.map(name => name).join(', ');
     galleryList.innerHTML += `
     <li class="gallery__item">
-        <div class="gallery__container"  id=${element.id} data-modal-open="">
-            <img
-                class="gallery__img"
-                src="https://image.tmdb.org/t/p/original${element.poster_path}"
-                alt="${element.media_type}"
-            />
-            </div>
-                <div class="gallery__description">
-                <h3 class="gallery__title" style="font-style:uppercase">${
-                  element.title
-                }</h3>
-                <p class="gallery__subtitle">${genres} | ${element.release_date.slice(
-      0,
-      4
-    )}</p>
-            </div>
-        </div>
+    <div class="gallery__container" id=${element.id} data-modal-open="">
+    <img
+    class="gallery__img"
+    src="https://image.tmdb.org/t/p/original${element.poster_path}"
+    alt="${element.title}"
+    />
+    </div>
+    <div class="gallery__description">
+    <h3 class="gallery__title" style="font-style:uppercase">${element.title}</h3>
+    <p class="gallery__subtitle">${element.genre_ids} | </p>
+    </div>
+    </div>
     </li>`;
   });
 });
 
 galleryList.addEventListener('click', event => {
-  const id = event.target.offsetParent.id || 0;
-  if (id === 0) return;
-  toggleModal(id);
+  const movieId = event.target.offsetParent.id || '0';
+  if (movieId === '0') return;
+  toggleModal(movieId);
 });
-/*fetchSameMovies('tears', 1);
-fetchMovieDetails(1217605);
-*/
--- src/js/modal-info.js
+++ src/js/modal-info.js
@@ -1,4 +1,5 @@
 import { fetchMovieDetails } from './api';
+import { addToWatched, addToQueue } from './add-movie';
 
 const refs = {
   openModalBtn: document.querySelector('[data-modal-open]'),
@@ -12,10 +13,19 @@ const refs = {
   genre: document.querySelector('#genre'),
   sinopsis: document.querySelector('#sinopsis'),
   poster: document.querySelector('#poster'),
+  btnWatched: document.querySelector('#watched-list'),
+  btnQueued: document.querySelector('#queue-list'),
+  addIdToWatched: event => {
+    addToWatched(event.target.value);
+  },
+  addIdToQueue: event => {
+    addToQueue(event.target.value);
+  },
 };
 
 refs.openModalBtn.addEventListener('click', toggleModal);
 refs.closeModalBtn.addEventListener('click', removeModal);
+
 window.addEventListener('keydown', event => {
   if (event.code === 'Escape') {
     removeModal();
@@ -24,12 +34,11 @@ window.addEventListener('keydown', event => {
 
 export function toggleModal(id) {
   refs.modal.classList.toggle('is-hidden');
-  if (id === 0) return;
   fetchMovieDetails(id)
     .then(response => {
-      console.log(response);
       refs.poster.src =
-        `https://image.tmdb.org/t/p/w400${response.poster_path}` || 'none';
+        `https://image.tmdb.org/t/p/w400${response.poster_path}` ||
+        '../images/not-found.jpg';
       refs.vote.textContent = response.vote_average || 'none';
       refs.votes.textContent = response.vote_count || 'none';
       refs.ogTitle.textContent = response.original_title || 'none';
@@ -40,13 +49,21 @@ export function toggleModal(id) {
         response.genres.map(({ name }) => name).length > 0
           ? response.genres.map(({ name }) => name).join(', ')
           : 'none';
+      refs.btnWatched.value = response.id;
+      refs.btnQueued.value = response.id;
     })
-    .catch(error => {});
+    .catch(error => console.log(error));
+
+  refs.btnWatched.addEventListener('click', refs.addIdToWatched);
+
+  refs.btnQueued.addEventListener('click', refs.addIdToQueue);
 }
+refs.modal.addEventListener('click', event => {
+  if (event.target.className !== 'backdrop') return;
+  removeModal();
+});
 export function removeModal() {
   refs.modal.classList.add('is-hidden');
+  refs.btnWatched.removeEventListener('click', refs.addIdToWatched);
+  refs.btnQueued.removeEventListener('click', refs.addIdToQueue);
 }
-/* 
-  poster_path: es el codigo de la imagen de poster
-  enlace para llamar imagen https://image.tmdb.org/t/p/original/${poster_path}
-*/
