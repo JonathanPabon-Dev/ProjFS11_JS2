a4b402905cef583006a9be5d44806f232cd34c71
JonathanPabon-Dev <jonathanpabon.dev@gmail.com>
2024-03-04 01:47:41 -0500

    Example files erased. Add jonathan_p photo

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -41,16 +41,14 @@ export function renderPaginator(totalPages, currentPage, mobile = false) {
       if (item.value === 'Prev') {
         button.id = 'prev-btn';
         button.classList.add('pg-btn__arrow');
-        button.innerHTML = `<svg width="16" height="16" viewBox="0 0 16 16" fill="none">
-                            <path d="M12.6666 8H3.33325" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round"/>
-                            <path d="M7.99992 12.6667L3.33325 8.00004L7.99992 3.33337" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round"/>
+        button.innerHTML = `<svg class="svg-close-modal">
+                              <use href="./images/symbol-defs.svg#filmoteka-left-arrow"></use>
                             </svg>`;
       } else if (item.value === 'Next') {
         button.id = 'next-btn';
         button.classList.add('pg-btn__arrow');
-        button.innerHTML = `<svg width="16" height="16" viewBox="0 0 16 16" fill="none">
-                            <path d="M3.33341 8H12.6667" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round"/>
-                            <path d="M8.00008 12.6667L12.6667 8.00004L8.00008 3.33337" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round"/>
+        button.innerHTML = `<svg class="svg-close-modal">
+                              <use href="./images/symbol-defs.svg#filmoteka-right-arrow"></use>
                             </svg>`;
       } else {
         button.textContent = item.value;
